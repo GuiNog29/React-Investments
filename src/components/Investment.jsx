1492c41889074fa301037c69ae2e6c7a18c24fae
Guilherme Almeida <guimathiasgui@gmail.com>
2021-08-25 23:23:22 -0300

    Mostrando valores em tela e formatando data

--- src/components/Investment.jsx
+++ src/components/Investment.jsx
@@ -1,3 +1,4 @@
+import { helperGetMonthDescription } from '../helpers/helpers';
 import MoneyFormatter from './MoneyFormatter';
 import PercentageFormatter from './PercentageFormatter';
 
@@ -14,12 +15,42 @@ export default function Investment({ children: investmentData }) {
       <h2 className="text-center font-semibold text-lg">{description}</h2>
 
       <h3 className="text-center font-semibold text-md">
-        Total Income: <span className={balanceColor}>
+        Total Income:{' '}
+        <span className={balanceColor}>
           <MoneyFormatter>{balance}</MoneyFormatter> (
-            <PercentageFormatter>{totalPercent}</PercentageFormatter>
-          )
+          <PercentageFormatter>{totalPercent}</PercentageFormatter>)
         </span>
       </h3>
+
+      <ul className="mt-4">
+        {reports.map(({ id, month, year, value, percentage }) => {
+          const valueColor =
+            percentage === 0
+              ? 'text-black'
+              : percentage > 0
+              ? 'text-green-600'
+              : 'text-red-600';
+
+          return (
+            <li
+              key={id}
+              className="flex flex-row items-center justify-between space-x-4 border-b"
+            >
+              <span className="font-mono">
+                {helperGetMonthDescription(month)} /{year}
+              </span>
+
+              <span className={`flex-grow ${valueColor}`}>
+                <MoneyFormatter>{value}</MoneyFormatter>
+              </span>
+
+              <span className={valueColor}>
+                <PercentageFormatter>{percentage}</PercentageFormatter>
+              </span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
